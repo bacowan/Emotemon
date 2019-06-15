81d1001046c0f65f82f18b3bdfc3daae7ef8e9bf
Brendan Cowan <bacowan@ualberta.ca>
2019-06-14 23:05:43 -0300

    downloading the list of emotes works

--- PokemonFireRed.cs
+++ PokemonFireRed.cs
@@ -4,17 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using CrowdControl.Common;
 using JetBrains.Annotations;
+using System.Web;
+using System.Net;
+using System.IO;
 using System.Reflection; // temp
+using Newtonsoft.Json;
 
 namespace CrowdControl.Games.Packs
 {
-	[UsedImplicitly]
-	public class PokemonFireRed : GBAEffectPack
+    [UsedImplicitly]
+    public class PokemonFireRed : GBAEffectPack
     {
+        private const string EmoteUrl = "https://api.twitch.tv/kraken/chat/emoticon_images";
+
         [NotNull]
         private readonly IPlayer _player;
 
-        public PokemonFireRed([NotNull] IPlayer player, [NotNull] Func<CrowdControlBlock, bool> responseHandler, [NotNull] Action<object> statusUpdateHandler) : base(responseHandler, statusUpdateHandler) => _player = player;
+        private IDictionary<string, string> emoteUrls;
+
+        public PokemonFireRed([NotNull] IPlayer player, [NotNull] Func<CrowdControlBlock, bool> responseHandler, [NotNull] Action<object> statusUpdateHandler) : base(responseHandler, statusUpdateHandler)
+        {
+            _player = player;
+
+            var webRequest = WebRequest.Create(EmoteUrl);
+            var response = webRequest.GetResponse();
+            using (var stream = response.GetResponseStream())
+            {
+                var reader = new StreamReader(stream);
+                var responseFromServer = reader.ReadToEnd();
+                emoteUrls = JsonConvert.DeserializeObject<AllEmotesResponse>(responseFromServer)
+                    .emoticons
+                    .GroupBy(r => r.code)
+                    .ToDictionary(r => r.Key, r => r.First().id);
+                List<SingleEmoteResponse> test;
+            }
+            response.Close();
+        }
 
         private volatile bool _quitting = false;
         public override void Dispose()
@@ -60,12 +85,29 @@ namespace CrowdControl.Games.Packs
                 return;
             }
 
-            MethodInfo[] methodInfos = Connector.GetType()
-                           .GetMethods(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var info in methodInfos)
-            {
-                Connector.SendMessage(info.Name);
-            }
+
+
+            //var constructors = typeof(Common.ItemType).GetConstructors();
+            //foreach (var constructor in constructors)
+            //{
+            //    foreach (var param in constructor.GetParameters())
+            //    {
+            //        Connector.SendMessage(param.Name + " " + param.ParameterType);
+            //    }
+            //    Connector.SendMessage("");
+            //}
+
+            //MethodInfo[] methodInfos = typeof(EffectRequest)
+            //               .GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            //foreach (var info in methodInfos)
+            //{
+            //    Connector.SendMessage(info.Name);
+            //}
+            //foreach (var info in Enum.GetNames(typeof(ItemKind)))
+            //{
+            //    Connector.SendMessage(info);
+            //}
+            Connector.SendMessage(emoteUrls.First().ToString());
 
             string[] codeParams = request.FinalCode.Split('_');
             TryEffect(request,
@@ -75,7 +117,7 @@ namespace CrowdControl.Games.Packs
                     //Connector.WriteWord(0x4000000, 0xAAAA);
                     Connector.SendMessage("Yay!");
                 });
-            
+
         }
 
         //private const ushort TEST = 0x08245F50;
@@ -87,6 +129,17 @@ namespace CrowdControl.Games.Packs
         }
 
         public override bool StopAllEffects() => base.StopAllEffects();
+        
+        private class AllEmotesResponse
+        {
+            public List<SingleEmoteResponse> emoticons { get; set; }
+        }
 
+        private class SingleEmoteResponse
+        {
+            public string code { get; set; }
+            public string emoticon_set { get; set; }
+            public string id { get; set; }
+        }
     }
 }
