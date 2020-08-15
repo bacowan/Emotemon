b53da7b12837cf3b5a8df353acb0734831005b2f
bacowan <bacowan@ualberta.ca>
2020-08-14 21:31:58 -0300

    setting up for release

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let win
 
 function createWindow() { 
    win = new BrowserWindow({width: 800, height: 600}) 
+   win.setMenu(null);
    win.loadURL(url.format ({ 
       pathname: path.join(__dirname, 'index.html'), 
       protocol: 'file:', 
